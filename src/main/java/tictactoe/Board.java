abc322c23b29633c55226b7e006025dd3709270a
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-02-06 12:47:05 -0500

    BoardTest: shouldHaveToStringAllX

--- src/main/java/tictactoe/Board.java
+++ src/main/java/tictactoe/Board.java
@@ -5,5 +5,9 @@ public class Board {
 	public Spot getSpot(String string, String string2) {
 		return new Spot("");
 	}
+	
+	public String toString() {
+		return "x|x|x\nx|x|x\nx|x|x";
+	}
 
 }
--- src/test/java/tictactoe/BoardTest.java
+++ src/test/java/tictactoe/BoardTest.java
@@ -1,15 +1,25 @@
 package tictactoe;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
 public class BoardTest {
 	
 	@Test
-	public void shouldHaveTopLeftBeSpot() {
+	public void shouldHaveTopLeft() {
 		Board underTest = new Board();
 		Spot spot = underTest.getSpot("Top", "Left");
 		assertNotNull(spot);
+	}
+	
+	@Test
+	public void shouldHaveToStringAllX() {
+		Board underTest = new Board();
+		String expected = "x|x|x\nx|x|x\nx|x|x";
+		String returned = underTest.toString();
+		assertThat(returned, is(expected));
 	}
 }
