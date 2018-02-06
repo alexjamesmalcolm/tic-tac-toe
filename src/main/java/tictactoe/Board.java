dc0932bb7478a7f60a3a04392a46f7e62ea192e6
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-02-06 13:09:06 -0500

    BoardTest: shouldHaveToStringAllO

--- src/main/java/tictactoe/Board.java
+++ src/main/java/tictactoe/Board.java
@@ -1,13 +1,40 @@
 package tictactoe;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Board {
+
+	private Map<Integer, Spot> board = new TreeMap<Integer, Spot>();
+
+	public Board(Map<Integer, String> board) {
+		for (int i = 0; i < 9; i++) {
+			String currentSymbol = board.get(i);
+			Spot spot = new Spot(currentSymbol);
+			this.board.put(i, spot);
+		}
+	}
+
+	public Board() {
+		this(new TreeMap<>());
+	}
 
 	public Spot getSpot(String string, String string2) {
 		return new Spot("");
 	}
-	
+
 	public String toString() {
-		return "x|x|x\nx|x|x\nx|x|x";
+		String output = "";
+		for (int i = 0; i < 9; i++) {
+			String currentSymbol = board.get(i).getSymbol();
+			output += currentSymbol;
+			if ((i + 1) % 3 == 0 && i < 8) {
+				output += "\n";
+			} else if (i < 8) {
+				output += "|";
+			}
+		}
+		return output;
 	}
 
 }
--- src/test/java/tictactoe/BoardTest.java
+++ src/test/java/tictactoe/BoardTest.java
@@ -4,22 +4,40 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 import org.junit.Test;
 
 public class BoardTest {
-	
+
 	@Test
 	public void shouldHaveTopLeft() {
 		Board underTest = new Board();
 		Spot spot = underTest.getSpot("Top", "Left");
 		assertNotNull(spot);
 	}
-	
+
 	@Test
 	public void shouldHaveToStringAllX() {
-		Board underTest = new Board();
+		Map<Integer, String> board = new TreeMap<Integer, String>();
+		for (int i = 0; i < 9; i++) {
+			board.put(i, "x");
+		}
+		Board underTest = new Board(board);
 		String expected = "x|x|x\nx|x|x\nx|x|x";
 		String returned = underTest.toString();
 		assertThat(returned, is(expected));
+	}
+
+	@Test
+	public void shouldHaveToStringAllO() {
+		Map<Integer, String> board = new TreeMap<Integer, String>();
+		for (int i = 0; i < 9; i++) {
+			board.put(i, "o");
+		}
+		Board underTest = new Board(board);
+		String returned = underTest.toString();
+		assertThat(returned, is("o|o|o\no|o|o\no|o|o"));
 	}
 }
